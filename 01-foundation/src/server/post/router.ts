fd85e74a15f02afe4789bc887ddfdd41138f75e9
Ruben Rubio Perucha <ruben.rubioperucha@plexus.es>
2024-10-30 12:46:22 +0100

    feat(01-foundation): Add create post route

--- 01-foundation/src/server/post/router.ts
+++ 01-foundation/src/server/post/router.ts
@@ -1,3 +1,4 @@
+import { z } from "zod";
 import { publicProcedure, router } from "../init";
 import service from "./services/post.service";
 
@@ -5,4 +6,21 @@ export const postRouter = router({
   getAll: publicProcedure.query(async () => {
     return service.getAll();
   }),
+  create: publicProcedure
+    .input(
+      z.object({
+        title: z.string(),
+        description: z.string(),
+        author: z.string(),
+        published: z.boolean(),
+      })
+    )
+    .mutation(async ({ input }) => {
+      try {
+        await service.create(input);
+      } catch (err) {
+        return { status: "error", error: err };
+      }
+      return { status: "success" };
+    }),
 });
