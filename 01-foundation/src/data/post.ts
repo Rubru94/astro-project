6f3b73e0823dbc1623b4764657197ae65e3cf86f
Ruben Rubio Perucha <ruben.rubioperucha@plexus.es>
2024-11-04 12:50:22 +0100

    feat(01-foundation): Defining types and some dummy data

---
+++ 01-foundation/src/data/post.ts
@@ -0,0 +1,25 @@
+import type { PostItem } from "./types";
+
+export const Posts: Array<PostItem> = [
+  {
+    id: 1,
+    title: "Example",
+    description: "Description",
+    author: "Jonh Doe",
+    published: true,
+  },
+  {
+    id: 2,
+    title: "Groove street",
+    description: "The real neighborhood",
+    author: "Carl Johnson",
+    published: true,
+  },
+  {
+    id: 3,
+    title: "Cities",
+    description: "Best places",
+    author: "Jonh Doe",
+    published: true,
+  },
+];
---
+++ 01-foundation/src/data/types.ts
@@ -0,0 +1,7 @@
+export type PostItem = {
+  id: number;
+  title: string;
+  description: string;
+  author: string;
+  published: boolean;
+};
